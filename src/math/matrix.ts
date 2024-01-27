fd4ed32ff1462e825b29535d41bbadf712a38c9b
pokir <pokir@users.noreply.github.com>
2024-01-27 21:32:02 +0100

    Add Matrix

---
+++ src/math/matrix.ts
@@ -0,0 +1,233 @@
+export class Matrix {
+  dimensions: [number, number]; // lines, columns
+
+  values: number[][];
+
+  constructor(dimensions: [number, number], values?: number[][]) {
+    this.dimensions = dimensions;
+
+    // null values by default
+    if (values === undefined) {
+      this.values = [];
+
+      for (let i = 0; i < this.dimensions[0]; i += 1) {
+        this.values.push([]);
+        for (let j = 0; j < this.dimensions[1]; j += 1) {
+          this.values[i].push(0);
+        }
+      }
+    } else {
+      if (values.length !== this.dimensions[0]) throw new Error('number of lines does not match dimensions');
+
+      values.forEach((line) => {
+        if (line.length !== this.dimensions[1]) throw new Error('number of columns does not match dimensions');
+      });
+
+      this.values = values;
+    }
+  }
+
+  clone() {
+    const values: number[][] = [];
+
+    // copy the values
+    this.values.forEach((line) => {
+      values.push([...line]);
+    });
+
+    return new Matrix(this.dimensions, values);
+  }
+
+  static identity(dimension: number) {
+    const matrix = new Matrix([dimension, dimension]);
+
+    for (let i = 0; i < dimension; i += 1) {
+      matrix.values[i][i] = 1;
+    }
+
+    return matrix;
+  }
+
+  add(matrix: Matrix) {
+    if (matrix.dimensions[0] !== this.dimensions[0] || matrix.dimensions[1] !== this.dimensions[1]) throw new Error('matrix dimensions do not match');
+
+    const result = this.clone();
+
+    for (let i = 0; i < result.dimensions[0]; i += 1) {
+      for (let j = 0; j < result.dimensions[1]; j += 1) {
+        result.values[i][j] += matrix.values[i][j];
+      }
+    }
+
+    return result;
+  }
+
+  multiply(factor: number) {
+    const result = this.clone();
+
+    for (let i = 0; i < result.dimensions[0]; i += 1) {
+      for (let j = 0; j < result.dimensions[1]; j += 1) {
+        result.values[i][j] *= factor;
+      }
+    }
+
+    return result;
+  }
+
+  subtract(matrix: Matrix) {
+    return this.add(matrix.multiply(-1));
+  }
+
+  divide(divisor: number) {
+    return this.multiply(1 / divisor);
+  }
+
+  product(matrix: Matrix) {
+    if (this.dimensions[1] !== matrix.dimensions[0]) throw new Error('matrix dimensions do not match');
+
+    const result = new Matrix([this.dimensions[0], matrix.dimensions[1]]);
+
+    for (let j = 0; j < matrix.dimensions[1]; j += 1) {
+      for (let i = 0; i < this.dimensions[0]; i += 1) {
+        for (let k = 0; k < this.dimensions[1]; k += 1) {
+          result.values[i][j] += this.values[i][k] * matrix.values[k][j];
+        }
+      }
+    }
+
+    return result;
+  }
+
+  transpose() {
+    const result = new Matrix([this.dimensions[1], this.dimensions[0]]);
+
+    for (let i = 0; i < this.dimensions[0]; i += 1) {
+      for (let j = 0; j < this.dimensions[1]; j += 1) {
+        result.values[j][i] = this.values[i][j];
+      }
+    }
+
+    return result;
+  }
+
+  augment(matrix: Matrix) {
+    // concatenate a matrix to the right
+    if (this.dimensions[0] !== matrix.dimensions[0]) throw new Error('matrix dimensions do not match');
+
+    return new Matrix(
+      [this.dimensions[0], this.dimensions[1] + matrix.dimensions[1]],
+      this.values.map((line, i) => [...line, ...matrix.values[i]]),
+    );
+  }
+
+  extract(startingLine: number, startingColumn: number, endingLine: number, endingColumn: number) {
+    if (startingLine < 0 || startingLine > this.dimensions[0]
+        || endingLine < 0 || endingLine > this.dimensions[0]
+        || startingColumn < 0 || startingColumn > this.dimensions[1]
+        || endingColumn < 0 || endingColumn > this.dimensions[1]
+        || endingLine <= startingLine || endingColumn <= startingColumn
+    ) throw new Error('invalid range');
+
+    const result = new Matrix([endingLine - startingLine, endingColumn - startingColumn]);
+    for (let i = 0; i < result.dimensions[0]; i += 1) {
+      for (let j = 0; j < result.dimensions[1]; j += 1) {
+        result.values[i][j] = this.values[startingLine + i][startingColumn + j];
+      }
+    }
+
+    return result;
+  }
+
+  private swapLines(line1: number, line2: number) {
+    const result = this.clone();
+
+    const temp = result.values[line1];
+    result.values[line1] = result.values[line2];
+    result.values[line2] = temp;
+
+    return result;
+  }
+
+  private multiplyLine(line: number, factor: number) {
+    const result = this.clone();
+
+    result.values[line] = result.values[line].map((value) => value * factor)
+      .map((value) => (value === 0 ? 0 : value)); // remove negative zeros
+
+    return result;
+  }
+
+  private addLine(sourceLine: number, targetLine: number, factor: number = 1) {
+    const result = this.clone();
+
+    result.values[targetLine] = result.values[targetLine]
+      .map((value, i) => value + result.values[sourceLine][i] * factor);
+
+    return result;
+  }
+
+  reduce(): {reduced: Matrix, pivots: number} {
+    let result = this.clone();
+
+    const pivotLines: number[] = []; // lines containing a pivot
+
+    // find pivots column by column
+    for (let j = 0; j < result.dimensions[1]; j += 1) {
+      let pivotLineIndex: number = -1;
+
+      for (let i = 0; i < result.dimensions[0]; i += 1) {
+        if (result.values[i][j] !== 0) {
+          if (pivotLineIndex === -1 && !pivotLines.includes(i)) {
+            // set this line as the pivot line
+            pivotLineIndex = i;
+            pivotLines.push(pivotLineIndex);
+
+            // divide the pivot line so the pivot is 1
+            const factor = 1 / result.values[pivotLineIndex][j];
+            result = result.multiplyLine(pivotLineIndex, factor);
+          } else if (pivotLineIndex !== -1 && i !== pivotLineIndex) {
+            // add the pivot line to the current line
+            const factor = -result.values[i][j];
+            result = result.addLine(pivotLineIndex, i, factor);
+          }
+        }
+      }
+    }
+
+    // simplify the pivot lines
+    for (let i = pivotLines.length - 2; i >= 0; i -= 1) {
+      // add multiples of each pivot line under it
+      for (let j = i + 1; j < pivotLines.length; j += 1) {
+        const pivotColumn = result.values[pivotLines[j]].findIndex((value) => value !== 0);
+
+        const factor = -result.values[pivotLines[i]][pivotColumn];
+        result = result.addLine(pivotLines[j], pivotLines[i], factor);
+      }
+    }
+
+    // put the pivot lines in order
+    for (let i = 0; i < pivotLines.length; i += 1) {
+      const pivotLine = pivotLines[i];
+
+      result = result.swapLines(i, pivotLine);
+
+      // if the line swapped was also a pivot line, update its index
+      if (pivotLines.includes(i)) {
+        pivotLines[pivotLines.indexOf(i)] = pivotLine;
+      }
+    }
+
+    return { reduced: result, pivots: pivotLines.length };
+  }
+
+  inverse() {
+    if (this.dimensions[0] !== this.dimensions[1]) throw new Error('cannot get the inverse of a non square matrix');
+
+    // check if it is invertible
+    const { pivots } = this.reduce();
+    if (pivots < this.dimensions[0]) throw new Error('matrix is not invertible');
+
+    const { reduced } = this.augment(Matrix.identity(this.dimensions[0])).reduce();
+    return reduced.extract(0, this.dimensions[1], this.dimensions[0], this.dimensions[1] * 2);
+  }
+}
--- src/math/vector.ts
+++ src/math/vector.ts
@@ -1,78 +1,58 @@
-export class Vector {
-  values: number[];
+import { Matrix } from './matrix.js';
 
+export class Vector extends Matrix {
   constructor(...values: number[]) {
-    /* values.forEach((value) => {
-      if (Number.isNaN(value)) throw new Error('not a number');
-    }); */
-
-    this.values = values;
+    super([values.length, 1], values.map((value) => [value]));
   }
 
-  clone() {
-    return new Vector(...this.values);
+  static fromMatrix(vectorAsMatrix: Matrix) {
+    if (vectorAsMatrix.dimensions[1] !== 1) throw new Error('matrix cannot be converted to vector as the dimensions do not match');
+
+    return new Vector(...vectorAsMatrix.values.map((line) => line[0]));
+  }
+
+  clone(): Vector {
+    return Vector.fromMatrix(Vector.fromMatrix(super.clone()));
+  }
+
+  getValues() {
+    return this.values.map((line) => line[0]);
   }
 
   get(index: number) {
-    return this.values[index];
+    return this.getValues()[index];
   }
 
   getDimension() {
-    return this.values.length;
+    return this.dimensions[0];
+  }
+
+  // overrride to return a vector instead of a matrix
+  add(vector: Vector) {
+    return Vector.fromMatrix(super.add(vector));
+  }
+
+  // overrride to return a vector instead of a matrix
+  subtract(vector: Vector) {
+    return Vector.fromMatrix(super.subtract(vector));
+  }
+
+  // overrride to return a vector instead of a matrix
+  multiply(factor: number) {
+    return Vector.fromMatrix(super.multiply(factor));
+  }
+
+  // overrride to return a vector instead of a matrix
+  divide(divisor: number) {
+    return Vector.fromMatrix(super.divide(divisor));
+  }
+
+  dot(vector: Vector) {
+    return vector.transpose().product(this).values[0][0];
   }
 
   getNorm() {
     return Math.sqrt(this.dot(this));
-  }
-
-  dot(vector: Vector) {
-    if (vector.getDimension() !== this.getDimension()) throw new Error('vector dimensions do not match');
-
-    return this.values.reduce((total, value, index) => (
-      total + value * vector.values[index]
-    ), 0);
-  }
-
-  add(vector: Vector) {
-    if (vector.getDimension() !== this.getDimension()) throw new Error('vector dimensions do not match');
-
-    const result = this.clone();
-
-    for (let i = 0; i < vector.values.length; i += 1) {
-      result.values[i] += vector.values[i];
-    }
-
-    return result;
-  }
-
-  subtract(vector: Vector) {
-    if (vector.getDimension() !== this.getDimension()) throw new Error('vector dimensions do not match');
-
-    const result = this.clone();
-
-    for (let i = 0; i < vector.values.length; i += 1) {
-      result.values[i] -= vector.values[i];
-    }
-
-    return result;
-  }
-
-  multiply(factor: number) {
-    // if (Number.isNaN(factor)) throw new Error('not a number');
-
-    const result = this.clone();
-    result.values = result.values.map((value) => value * factor);
-    return result;
-  }
-
-  divide(divisor: number) {
-    if (divisor === 0) throw new Error('division by zero');
-
-    // if (Number.isNaN(divisor)) throw new Error('not a number');
-
-    const result = this.clone();
-    result.values = result.values.map((value) => value / divisor);
-    return result;
   }
 
   normalize() {
@@ -83,24 +63,26 @@ export class Vector {
     return this.divide(norm);
   }
 
+  // TODO: move to separate file: Vector3 ?
   cross(vector: Vector) {
     if (this.getDimension() !== 3 || vector.getDimension() !== 3) throw new Error('cross product on vectors with dimensions not equal to 3');
 
     return new Vector(
-      this.values[1] * vector.values[2] - this.values[2] * vector.values[1],
-      this.values[0] * vector.values[2] - this.values[2] * vector.values[0],
-      this.values[0] * vector.values[1] - this.values[1] * vector.values[0],
+      this.get(1) * vector.get(2) - this.get(2) * vector.get(1),
+      this.get(0) * vector.get(2) - this.get(2) * vector.get(0),
+      this.get(0) * vector.get(1) - this.get(1) * vector.get(0),
     );
   }
 
+  // TODO: move to separate file: Quaternion
   hamilton(vector: Vector) {
     if (this.getDimension() !== 4 || vector.getDimension() !== 4) throw new Error('hamilton product on vectors with dimensions not equal to 4');
 
     return new Vector(
-      this.values[0] * vector.values[0] - this.values[1] * vector.values[1] - this.values[2] * vector.values[2] - this.values[3] * vector.values[3],
-      this.values[0] * vector.values[1] + this.values[1] * vector.values[0] + this.values[2] * vector.values[3] - this.values[3] * vector.values[2],
-      this.values[0] * vector.values[2] - this.values[1] * vector.values[3] + this.values[2] * vector.values[0] + this.values[3] * vector.values[1],
-      this.values[0] * vector.values[3] + this.values[1] * vector.values[2] - this.values[2] * vector.values[1] + this.values[3] * vector.values[0],
+      this.get(0) * vector.get(0) - this.get(1) * vector.get(1) - this.get(2) * vector.get(2) - this.get(3) * vector.get(3),
+      this.get(0) * vector.get(1) + this.get(1) * vector.get(0) + this.get(2) * vector.get(3) - this.get(3) * vector.get(2),
+      this.get(0) * vector.get(2) - this.get(1) * vector.get(3) + this.get(2) * vector.get(0) + this.get(3) * vector.get(1),
+      this.get(0) * vector.get(3) + this.get(1) * vector.get(2) - this.get(2) * vector.get(1) + this.get(3) * vector.get(0),
     );
   }
 }
